485f940ac21732526ac729699419a6cd3bf94609
jnappy <jpnappy@gmail.com>
2015-03-23 18:58:04 -0400

    gave user what hte user was if they did not win

--- jay_nappy/secret_number.rb
+++ jay_nappy/secret_number.rb
@@ -2,17 +2,17 @@
 
 def check_users_number
   tries_left = 3 
-  random_number = 1 + rand(10)
+  @random_number = 1 + rand(10)
   puts "Your SECRET NUMBAH has been chosen - guess a numbah between 1 and 10!"
   while tries_left > 0 
     player_guess = gets.strip.to_i
-    if player_guess == random_number
-      puts "OH MY GAWD YOU GUESSED THE SECRET NUMBAH, #{random_number}!  YOU WIN!"
+    if player_guess == @random_number
+      puts "OH MY GAWD YOU GUESSED THE SECRET NUMBAH, #{@random_number}!  YOU WIN!"
       @did_they_win = true
       break 
     elsif tries_left > 0
       tries_left -=1
-      if player_guess > random_number
+      if player_guess > @random_number
       puts "You guessed too high, silly! You have #{tries_left} guesses before the game is over enter a another number" if tries_left > 0     
       else 
       puts "You guessed too low, silly! You have #{tries_left} guesses before the game is over enter a another number" if tries_left > 0     
@@ -27,7 +27,7 @@ puts "Hello there, #{player_name}.  Here are the rules:
 1. You will try to guess a number between 1 and 10
 2. You have three tries"
 check_users_number
-puts "You didn't guess! Better luck next time!" if @did_they_win != true
+puts "You didn't guess the numbah.  It was #{@random_number}! Better luck next time!" if @did_they_win != true
 #run number checker method / loop
 
 # Once completed, save the file, commit the code to Git and push it to GitHub:
