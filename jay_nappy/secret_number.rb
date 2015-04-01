5d46e6761c72f87357cc209e58090694d84bed8a
jnappy <jpnappy@gmail.com>
2015-04-01 14:41:17 -0400

    integrated mahkness

--- jay_nappy/secret_number.rb
+++ jay_nappy/secret_number.rb
@@ -1,4 +1,4 @@
-
+require 'pry'
 
 def check_users_number
   tries_left = 3 
@@ -21,8 +21,42 @@ def check_users_number
   end 
 end
 
-puts "Welcome to SECRET NUMBAH! This game has been created by Jay Nappy © 2015. Please tell us your name."
-player_name = gets.strip 
+def is_name_mahk (name)
+  if name == "mahk" || name == "mark"
+
+    system(" say 'Oh my gawd' ")
+    sleep(1)
+    system("say 'Your name is mahk'")
+    sleep(1)
+    system("say 'that is the best name ever'")
+    sleep(1)
+    system( "say 'Mahk' ")
+    sleep(1)
+    system( "say 'Mahk' ")
+    sleep(1)
+    system( "say 'glutten free Mahk' ")
+    sleep(1)
+    system( "say 'Mahk' ")
+    sleep(0.5)
+    system( "say 'Saint Mahk' ")
+    sleep(0.5)
+    system( "say 'Mahk' ")
+    sleep(0.5)
+    system( "say 'Mahk' ")
+    sleep(0.5)
+    system("say 'Mahk Mahk Mahk  Mahk Mahk Skid Mahk Mahk Mahk Mahk Mahk Mahk Mahk Mahk Mahk Mahk Mahk Mahk Mahk Mahk Mahk Mahk Mahk Mahk Mahk Mahk Mahk Mahk Mahk Mahk Mahk Mahk Mahk Mahk Mahk Mahk'")
+    sleep(1)
+    system("say 'happy april fools day mahk!'")
+
+    exit 
+  end
+end
+
+
+puts "Welcome to SECRET NUMBAH! This game has been created by Jay Nappy © 2015. Please enter your first name."
+player_name = gets.strip.downcase
+
+is_name_mahk(player_name)
 puts "Hello there, #{player_name}.  Here are the rules: 
 1. You will try to guess a number between 1 and 10
 2. You have three tries"
