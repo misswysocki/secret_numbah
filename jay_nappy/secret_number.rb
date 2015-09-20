7ef244f9d2a8a858d8c798aa59221801c2e3b489
jnappy <jpnappy@gmail.com>
2015-09-20 14:20:03 -0400

    solution to secret number

--- jay_nappy/secret_number.rb
+++ jay_nappy/secret_number.rb
@@ -1,5 +1,3 @@
-require 'pry'
-
 def check_users_number
   tries_left = 3 
   @random_number = 1 + rand(10)
@@ -21,54 +19,10 @@ def check_users_number
   end 
 end
 
-def is_name_mahk (name)
-  if name == "mahk" || name == "mark"
-
-    system(" say 'Oh my gawd' ")
-    sleep(1)
-    system("say 'Your name is mahk'")
-    sleep(1)
-    system("say 'that is the best name ever'")
-    sleep(1)
-    system( "say 'Mahk' ")
-    sleep(1)
-    system( "say 'Mahk' ")
-    sleep(1)
-    system( "say 'glutten free Mahk' ")
-    sleep(1)
-    system( "say 'Mahk' ")
-    sleep(0.5)
-    system( "say 'Saint Mahk' ")
-    sleep(0.5)
-    system( "say 'Mahk' ")
-    sleep(0.5)
-    system( "say 'Mahk' ")
-    sleep(0.5)
-    system("say 'Mahk Mahk Mahk  Mahk Mahk Skid Mahk Mahk Mahk Mahk Mahk Mahk Mahk Mahk Mahk Mahk Mahk Mahk Mahk Mahk Mahk Mahk Mahk Mahk Mahk Mahk Mahk Mahk Mahk Mahk Mahk Mahk Mahk Mahk Mahk Mahk'")
-    sleep(1)
-    system("say 'happy april fools day mahk!'")
-
-    exit 
-  end
-end
-
-
-puts "Welcome to SECRET NUMBAH! This game has been created by Jay Nappy © 2015. Please enter your first name."
-player_name = gets.strip.downcase
-
-is_name_mahk(player_name)
+puts "Welcome to SECRET NUMBAH! This game has been created by Jay Nappy © 2015. Please tell us your name."
+player_name = gets.strip 
 puts "Hello there, #{player_name}.  Here are the rules: 
 1. You will try to guess a number between 1 and 10
 2. You have three tries"
 check_users_number
 puts "You didn't guess the numbah.  It was #{@random_number}! Better luck next time!" if @did_they_win != true
-#run number checker method / loop
-
-# Once completed, save the file, commit the code to Git and push it to GitHub:
-
-# ```bash
-# cd ~/Sites/secret_number/your_name_here/
-# git add secret_number.rb
-# git commit -m "Adding secret number homework"
-# git push origin master
-# ```
